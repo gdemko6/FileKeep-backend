befd4cb3dadea148316f77bb1c778e676ca66612
Greg Demko <gregory.demko@my.uwrf.edu>
2025-05-27 16:49:23 -0500

    Change port to env variable

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.get("/", (req, res) => {
   res.json({ message: "API is running" });
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
